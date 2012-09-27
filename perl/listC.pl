93d618552f869e01a031f4ec8a5d6ef8b6928622
Shane McIntosh <shanemcintosh@acm.org>
2012-09-27 10:55:41 -0400

    Write word sequences to a database

--- perl/listC.pl
+++ perl/listC.pl
@@ -4,13 +4,12 @@ use Compress::LZF;
 use DBI;
 
 my @keywords = ("abstract", "continue", "for", "new", "switch", "assert",
-                "default", "goto", "package", "synchronized", "boolean", "do", "if",
-                "private", "this", "break", "double", "implements", "protected", "throw",
-                "byte", "else", "import", "public", "throws", "case", "enum",
-                "instanceof", "return", "transient", "catch", "extends", "int", "short",
-                "try", "char", "final", "interface", "static", "void", "class",
-                "finally", "long", "strictfp", "volatile", "const", "float", "native",
-                "super", "while");
+    "default", "goto", "package", "synchronized", "boolean", "do", "if",
+    "private", "this", "break", "double", "implements", "protected", "throw",
+    "byte", "else", "import", "public", "throws", "case", "enum", "instanceof",
+    "return", "transient", "catch", "extends", "int", "short", "try", "char",
+    "final", "interface", "static", "void", "class", "finally", "long",
+    "strictfp", "volatile", "const", "float", "native", "super", "while");
 
 my $b = new DB_File::HASHINFO;
 $b ->{cachesize}=1000000000;
@@ -24,27 +23,33 @@ tie %clones, "DB_File", $fname, O_RDONLY, 0666, $b
 
 # TODO: Make DB path configurable
 my $dbh = DBI->connect("dbi:SQLite:dbname=/scratch1/shane/cisc8xx/replication/ids", "", "") || die "Cannot connect: $DBI::errstr";
+my $odbh = DBI->connect("dbi:SQLite:dbname=/scratch1/shane/cisc8xx/replication/word_seqs.db", "", "") || die "Cannot connect: $DBI::errstr";
+
+# Improve performance
+$odbh->do("PRAGMA synchronous = OFF");
 
 while (my ($codec, $vs) = each %clones){
     my $res = $dbh->selectall_arrayref("SELECT type FROM ids WHERE id = \"$vs\"");
     if (@$res->[0][0] == 'C' || @$res->[0][0] == 'H' || @$res->[0][0] == 'J') {
         my $code = decompress $codec;
 
-    	print "PRINTING METHOD HEADERS\n\n";
+    	print "$vs: methods\n";
 
     	# Looks for method headers and break them up into words
     	while ($code =~ /([A-Za-z0-9_]+)[ \t\n\r]*\(.*\)[ \t\n\r]*(throws[ \t\n\r]+[A-Za-z0-9_]+[ \t\n\r]*)?\{.*\}*/g) {
 			if (!grep( /$1/, @keywords)) {
 				# DeCamel them or seperate underscore words
             	my @lineParsed = split(/(?<!^)(?=[A-Z])|_/,$1);
-				for my $i ( 0 .. $#lineParsed){
-               		print "$lineParsed[$i],";
-            	}
-            	print "\n";
+                my $lower = lc("@lineParsed");
+                $lower =~ s/^\s+//; # remove leading spaces
+                $lower =~ s/\s+$//; # remove trailing spaces
+                unless ($lower =~ /^\s*$/) {
+                    $odbh->do("INSERT INTO word_seqs VALUES ('D', \"$vs\", \"$lower\")");
+                }
         	}
     	}
 
-    	print "PRINTING COMMENTS\n\n";
+    	print "$vs: comments\n";
 
         # Looks for comments in code and break them up into words
         while ($code =~ /((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/g) {
@@ -59,18 +64,19 @@ while (my ($codec, $vs) = each %clones){
             #Split each of the sentences into words
             for my $i ( 0 .. $#sentences){
                 my @words = split(/[\t\n\r ]+/,$sentences[$i]);
-
-                #Print each word seperated by commas
-                for my $j (0 .. $#words){
-                    print "$words[$j],";
+                my $lower = lc("@words");
+                $lower =~ s/^\s+//; # remove leading spaces
+                $lower =~ s/\s+$//; # remove trailing spaces
+                unless ($lower =~ /^\s*$/) {
+                    $odbh->do("INSERT INTO word_seqs VALUES ('M', \"$vs\", \"$lower\")");
                 }
-                print "\n";
             }
         }
     } else {
-		print "SKIPPING\n";
+		print "$vs: skipping\n";
     }
 }
 untie %clones;
 
 $dbh->disconnect;
+$odbh->disconnect;
