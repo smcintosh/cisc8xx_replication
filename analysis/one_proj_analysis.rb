fe94c4b366ee2b2b26701f44f8e67cd545936994
Shane McIntosh <shanemcintosh@acm.org>
2012-10-18 21:47:16 -0400

    Added a corny, one project analysis script

---
+++ analysis/one_proj_analysis.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+require 'rubygems'
+require 'stemmer'
+require 'set'
+require './WordSequenceDatabase.rb'
+require './SeqComparator.rb'
+require './Cutoffs.rb'
+require './hacks.rb'
+
+#
+# MAIN LOOP
+#
+
+db = WordSequenceDatabase.new("/scratch3/shane/word_seqs.db.back")
+db.for_project("a2ps") do |pdata|
+    puts "Processing data"
+    STDOUT.flush
+    pdata.process
+end
+
+db.close
